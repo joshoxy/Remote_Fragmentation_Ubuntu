198e61595f284198655392a266bc265851b635d6
joshoxy <joshoxymoron@gmail.com>
2022-05-08 01:54:02 +0300

    Formatting

--- SiteTwo_SqLite.py
+++ SiteTwo_SqLite.py
@@ -20,20 +20,23 @@ local_stream = mysql.connector.connect(
 local_cursor = local_stream.cursor(buffered=True)
                     
 # Creating tables
-#cursor_obj.execute("CREATE TABLE doctors (ID INTEGER PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255), phone INT, department VARCHAR(255), salary INT, employment_date DATE)")
-#print("Table successfully created")
+cursor_obj.execute("CREATE TABLE IF NOT EXISTS doctors (ID INTEGER PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255), phone INT, department VARCHAR(255), salary INT, employment_date DATE)")
+print("Doctor's table successfully created")
+print("")
 
 #Check all the available tables in the sqlite database
 # cursor_obj.execute("SELECT name FROM sqlite_master WHERE type='table';")
 # print(cursor_obj.fetchall())
 
 print("Hybrid Fragmentation on sqlite: ")
+print("")
 print("Query one (>100,000): ")
 
 local_cursor.execute("SELECT ID, first_name, salary FROM doctors WHERE salary > 100000")
 local_query_result = local_cursor.fetchall()
 print("Fragment fetched from localhost: ")
 print(local_query_result)
+print("")
 
 #Insert data fetched into sqlite database
 
@@ -45,13 +48,13 @@ cursor_obj.executemany(insert_query, local_query_result)
 connection_obj.commit()
 
 #Display the fragment created on the sqlite database
-#updated_query = "SELECT ID, first_name, salary FROM doctors WHERE salary > 100000"
-#cursor_obj.execute(updated_query)
 cursor_obj.execute("CREATE TABLE IF NOT EXISTS salary_1 AS SELECT * FROM doctors WHERE salary > 100000")
 cursor_obj.execute("SELECT ID, first_name, salary FROM salary_1")
 updated_query_result = cursor_obj.fetchall()
 print("Fragment allocated to Sqlite: salary_1 table (>100000) ")
 print(updated_query_result)
+print("")
+#End of query one
 
 print("Query two (<100,000): ")
 
@@ -59,38 +62,36 @@ local_cursor.execute("SELECT ID, first_name, salary FROM doctors WHERE salary < 
 local_query_result2 = local_cursor.fetchall()
 print("Fragment fetched from localhost: ")
 print(local_query_result2)
+print("")
 
 #Insert data fetched into sqlite database
 
 #First clear the table to ensure no duplicates
-cursor_obj.execute("DELETE FROM doctors")  #clear table before inserting to remobve duplicates
+cursor_obj.execute("DELETE FROM doctors")  #clear table before inserting to remoove duplicates
 insert_query2 = "INSERT INTO doctors (ID, first_name, salary) VALUES (?,?,?)"
 cursor_obj.executemany(insert_query2, local_query_result2)
 connection_obj.commit()
 
 #Display the fragment created on the sqlite database
-#updated_query2 = "SELECT ID, first_name, salary FROM doctors WHERE salary < 100000"
-#cursor_obj.execute(updated_query2)
 cursor_obj.execute("CREATE TABLE IF NOT EXISTS salary_2 AS SELECT * FROM doctors WHERE salary < 100000")
 cursor_obj.execute("SELECT ID, first_name, salary FROM salary_2")
 updated_query_result2 = cursor_obj.fetchall()
 print("Fragment allocated to Sqlite: salary_2 table (<100000)")
 print(updated_query_result2)
+print("")
 
 #Query three
 print("Query three: (Gender: Male) ")
 
 #Create patients table
-#cursor_obj.execute("CREATE TABLE patients (ID INTEGER PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255), phone INT, gender VARCHAR(255), location VARCHAR(255), DOB DATE)")
+cursor_obj.execute("CREATE TABLE IF NOT EXISTS patients (ID INTEGER PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255), phone INT, gender VARCHAR(255), location VARCHAR(255), DOB DATE)")
 
-#Check if table has been created
-# cursor_obj.execute("SELECT name FROM sqlite_master WHERE type='table';")
-# print(cursor_obj.fetchall())
-
+#fetch from local host
 local_cursor.execute("SELECT ID, first_name, last_name, gender FROM patients WHERE gender = 'Male'")
 local_query_result1 = local_cursor.fetchall()
 print("Fragment fetched from localhost: ")
 print(local_query_result1)
+print("")
 
 #Insert data fetched into sqlite database
 
@@ -102,28 +103,22 @@ cursor_obj.executemany(insert_query1, local_query_result1)
 connection_obj.commit()
 
 #Display the fragment created on the sqlite database
-#updated_query1 = "SELECT ID, first_name, last_name, gender FROM patients WHERE gender = 'Male'"
-#cursor_obj.execute(updated_query1)
 cursor_obj.execute("CREATE TABLE IF NOT EXISTS male AS SELECT * FROM patients WHERE gender = 'Male'")
 cursor_obj.execute("SELECT ID, first_name, last_name, gender FROM male")
 updated_query_result1 = cursor_obj.fetchall()
 print("Fragment allocated to Sqlite: male table ")
 print(updated_query_result1)
+print("")
 
 #Query four
 print("Query four: (Gender: NOT Male) ")
 
-#Create patients table
-#cursor_obj.execute("CREATE TABLE patients (ID INTEGER PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255), phone INT, gender VARCHAR(255), location VARCHAR(255), DOB DATE)")
-
-#Check if table has been created
-# cursor_obj.execute("SELECT name FROM sqlite_master WHERE type='table';")
-# print(cursor_obj.fetchall())
-
+#fetch from localhost
 local_cursor.execute("SELECT ID, first_name, last_name, gender FROM patients WHERE gender != 'Male'")
 local_query_result3 = local_cursor.fetchall()
 print("Fragment fetched from localhost: ")
 print(local_query_result3)
+print("")
 
 #Insert data fetched into sqlite database
 
@@ -135,13 +130,12 @@ cursor_obj.executemany(insert_query3, local_query_result3)
 connection_obj.commit()
 
 #Display the fragment created on the sqlite database
-#updated_query3 = "SELECT ID, first_name, last_name, gender FROM patients WHERE gender != 'Male'"
-#cursor_obj.execute(updated_query3)
 cursor_obj.execute("CREATE TABLE IF NOT EXISTS not_male AS SELECT * FROM patients WHERE gender != 'Male'")
 cursor_obj.execute("SELECT ID, first_name, last_name, gender FROM not_male")
 updated_query_result3 = cursor_obj.fetchall()
-print("Fragment allocated to Sqlite: not_name table ")
+print("Fragment allocated to Sqlite: not_male table ")
 print(updated_query_result3)
+print("")
 
 
 # Close the connection
--- main_mysql.py
+++ main_mysql.py
@@ -13,66 +13,77 @@ dataBase = mysql.connector.connect(
 # preparing a cursor object
 cursorObject = dataBase.cursor(buffered=True)
 
-print("Derived Horizontal")  #loc1 table picks admissions where location = nairobi
-#print("LOC1:")
-#Create fragment loc1 where location is Nairobi
-#cursorObject.execute("CREATE TABLE LOC1 (SELECT * FROM admissions WHERE location = 'Nairobi')")  #Query to create fragment LOC1 WHERE location = 'Nairobi";
-# query2 = "SELECT * FROM admissions INNER JOIN loc1 ON admissions.admission_id = loc1.admission_id WHERE loc1.ward_name = 'White House'"
-# cursorObject.execute(query2)
+print("Derived Horizontal") 
+print("")
+ #Query to create fragment LOC1 WHERE location = Nairobi ";
+cursorObject.execute("CREATE TABLE IF NOT EXISTS loc1 (SELECT * FROM admissions WHERE location = 'Nairobi')")
+cursorObject.execute("SELECT * FROM loc1")
+loc1_result = cursorObject.fetchall()
+print("LOC1: location = Nairobi' ")
+print(loc1_result)
+print("")
 
 #create white_house_table fragment by doing inner join on admissions and loc1
 cursorObject.execute("CREATE TABLE IF NOT EXISTS white_house_table (SELECT admissions.admission_id, admissions.ward_name, admissions.patient_id FROM admissions INNER JOIN loc1 ON admissions.admission_id = loc1.admission_id WHERE loc1.ward_name = 'White House')")
 cursorObject.execute("SELECT * FROM white_house_table")
 query_two_result = cursorObject.fetchall()
-
-print("white_house_table: ")
+print("white_house_table: admissions x loc1 ")
 print(query_two_result)
+print("")
+#End
 
-# print("LOC2:")  #loc2 table picks admissions where location != nairobi
-# cursorObject.execute("CREATE TABLE LOC2 (SELECT * FROM admissions WHERE location != 'Nairobi')")  #Query to create fragment LOC2 WHERE location != 'Nairobi";
-# query3 = "SELECT * FROM admissions INNER JOIN loc2 ON admissions.admission_id = loc2.admission_id WHERE loc2.ward_name = 'St Peters'"
-# cursorObject.execute(query3)
-# query_three_result = cursorObject.fetchall()
-# print(query_three_result)
-#original
+
+#Query to create fragment LOC2 WHERE location != 'Nairobi";
+cursorObject.execute("CREATE TABLE IF NOT EXISTS loc2 (SELECT * FROM admissions WHERE location != 'Nairobi')")
+cursorObject.execute("SELECT * FROM loc2")
+loc2_result = cursorObject.fetchall()
+print("LOC2: location != 'Nairobi' ")
+print(loc2_result)
+print("")
+
 
 #create st_peters fragment by doing innrer join on admissions and loc2
 cursorObject.execute("CREATE TABLE IF NOT EXISTS st_peters_table (SELECT admissions.admission_id, admissions.ward_name, admissions.patient_id FROM admissions INNER JOIN loc2 ON admissions.admission_id = loc2.admission_id WHERE loc2.ward_name = 'St Peters')")
 cursorObject.execute("SELECT * FROM st_peters_table")
 query_three_result = cursorObject.fetchall()
-
-print("st_peters_table: ")
+print("st_peters_table: admissions x loc2 ")
 print(query_three_result)
+print("")
+#End
 
 
 print("Veritcal")
-print("Q1 = :")
+print("Q1 = ID, first_name, location FROM patients: ")
 query4 = "SELECT ID, first_name, location FROM patients"
 #cursorObject.execute("CREATE TABLE patients_q1 (SELECT ID, first_name, location FROM patients)")
 cursorObject.execute(query4)
 query_four_result = cursorObject.fetchall()
 print(query_four_result)
+print("")
 
-print("Q2 = :")
+print("Q2 = admission_id, patient_id, ward_name FROM admissions: ")
 query5 = "SELECT admission_id, patient_id, ward_name FROM admissions"
 #cursorObject.execute("CREATE TABLE ward_q2 (SELECT admission_id, patient_id, ward_name FROM admissions)")
 cursorObject.execute(query5)
 query_five_result = cursorObject.fetchall()
 print(query_five_result)
+print("")
 
-print("Q3 = :")
+print("Q3 = doctor_id, ward_name FROM admissions:")
 query6 = "SELECT doctor_id, ward_name FROM admissions"
 #cursorObject.execute("CREATE TABLE ward_q3 (SELECT doctor_id, ward_name FROM admissions)")
 cursorObject.execute(query6)
 query_six_result = cursorObject.fetchall()
 print(query_six_result)
+print("")
 
-print("Q4 = :")
+print("Q4 = ID, first_name FROM patients WHERE location = 'Nairobi' :")
 query7 = "SELECT ID, first_name FROM patients WHERE location = 'Nairobi'"
 #cursorObject.execute("CREATE TABLE patients_q4 (SELECT ID, first_name FROM patients WHERE location = 'Nairobi')")
 cursorObject.execute(query7)
 query_seven_result = cursorObject.fetchall()
 print(query_seven_result)
+print("")
 
 myresult = cursorObject.fetchall()
   
